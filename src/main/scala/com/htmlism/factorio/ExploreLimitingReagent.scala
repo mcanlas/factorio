a04b4574abaa2bd9c04dd2535dc0341297a45a90
Mark Canlas <github@htmlism.com>
2024-11-01 20:59:47 -0400

    flatmap

--- src/main/scala/com/htmlism/factorio/ExploreLimitingReagent.scala
+++ src/main/scala/com/htmlism/factorio/ExploreLimitingReagent.scala
@@ -1,5 +1,6 @@
 package com.htmlism.factorio
 
+import cats.*
 import cats.data.*
 import cats.effect.*
 import cats.syntax.all.*
@@ -7,7 +8,9 @@ import io.circe.generic.auto.*
 
 import com.htmlism.common.*
 
-object ExploreLimitingReagent extends ExploreLimitingReagent[IO] with IOApp.Simple:
+object ExploreLimitingReagent
+    extends ExploreLimitingReagent[IO](YamlResourceLoader(ResourceFileLoader[IO]))
+    with IOApp.Simple:
   def rateOfProduction(name: String, oracle: Oracle): Double =
     oracle.recipe(name) match
       case Some(r) =>
@@ -23,10 +26,7 @@ object ExploreLimitingReagent extends ExploreLimitingReagent[IO] with IOApp.Simp
       case None =>
         oracle.material(name).miningSpeed
 
-class ExploreLimitingReagent[F[_]](using F: Sync[F]):
-  private val yamlReader =
-    YamlResourceLoader(ResourceFileLoader[F])
-
+class ExploreLimitingReagent[F[_]: FlatMap](yamlReader: YamlResourceLoader[F]):
   def run: F[Unit] =
     for
       materials <- yamlReader
