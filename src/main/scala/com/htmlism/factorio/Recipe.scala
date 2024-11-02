f33484b5d59fec8884f8b47a51940da1e411391b
Mark Canlas <github@htmlism.com>
2024-11-01 20:30:26 -0400

    crafting time

--- src/main/scala/com/htmlism/factorio/Recipe.scala
+++ src/main/scala/com/htmlism/factorio/Recipe.scala
@@ -2,7 +2,7 @@ package com.htmlism.factorio
 
 import cats.data.NonEmptyList
 
-case class Recipe(name: String, time: Double, requirements: NonEmptyList[Recipe.Requirement])
+case class Recipe(name: String, craftingTime: Double, requirements: NonEmptyList[Recipe.Requirement])
 
 object Recipe:
   case class Requirement(name: String, quantity: Double)
