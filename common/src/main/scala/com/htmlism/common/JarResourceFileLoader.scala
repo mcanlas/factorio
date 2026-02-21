891992492127063341f3425b627c24d07195b24f
Mark Canlas <github@htmlism.com>
2026-02-20 20:45:22 -0500

    syntax

--- common/src/main/scala/com/htmlism/common/JarResourceFileLoader.scala
+++ common/src/main/scala/com/htmlism/common/JarResourceFileLoader.scala
@@ -3,7 +3,6 @@ package com.htmlism.common
 import java.io.InputStream
 
 import cats.effect._
-import cats.syntax.all._
 
 class JarResourceFileLoader[F[_]](using F: Sync[F]):
   def load(path: String): Resource[F, InputStream] =
